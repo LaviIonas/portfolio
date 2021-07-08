537d978657faee35e15840c7dfbcc548f03184b5
lavi <laviionas772@gmail.com>
2021-07-08 05:06:18 -0400

    doesnt work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component, PropTypes} from 'react';
+
+import { wheel } from './wheel.js';
+
+class MyApp extends Component {
+  constructor(){
+    super();
+    this.state = {};
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <script src="https://unpkg.com/konva@8/konva.min.js"></script>
+      <header className="wheel">
+
       </header>
+        <body>
+          <script>
+            wheel()
+          </script>
+        </body>
     </div>
   );
 }
